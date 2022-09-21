58809dc59d5dffc9cd1e31f63e61de259ea0992a
AndreySHSH <laptev.andrey@icloud.com>
2022-09-21 03:42:31 +0300

    add auth

--- src/components/app.js
+++ src/components/app.js
@@ -41,17 +41,19 @@ class App extends Component{
 
             let state = this.state.store.getState()
 
-            if (state.error.showError) {
-                toast.error(state.error.errorText, {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                });
-            }
+            console.log(state)
+
+            // if (state.error.showError) {
+            //     toast.error(state.error.errorText, {
+            //         position: "top-right",
+            //         autoClose: 5000,
+            //         hideProgressBar: false,
+            //         closeOnClick: true,
+            //         pauseOnHover: true,
+            //         draggable: true,
+            //         progress: undefined,
+            //     });
+            // }
         })
     }
 

85a3722acda24efa52ab2364d823d389545a9d8a
Андрей Шмелев-Шампанов <andreyshsh@MacBook-Air-Andrej.local>
2022-09-23 06:03:02 +0300

    add mobile web view

--- src/components/user.js
+++ src/components/user.js
@@ -32,15 +32,9 @@ class User extends Component{
                             :
                                 store.user.execute === true ?
                                     <div className="block-default-pre">
-                                        <div className="title-block">Список заданий</div>
+                                        <div className="title-block">Ваши задания</div>
                                         <div className="navigation-preview">
-                                            {/*<div className="block-text-pre">*/}
-                                            {/*    Раскрутка сообществ VK актуальная тема в 2022 году. Сегодня можно увидеть тысячи различных сообществ разной тематики. Одни создают группу как хобби, другие для продажи товаров и предоставления услуг, третьи для заработка на рекламе.*/}
-                                            {/*    Чтобы Ваше сообщество стало заметным и популярным нужно приложить время и усилия. Вы должны придерживаться наших рекомендаций и раскрутить группу в VK станет намного проще, эффективнее и быстрее.*/}
-                                            {/*</div>*/}
-                                            {/*<div className="block-default-icon">*/}
-                                            {/*    <img className="default-icon" src={vk} alt="vk"/>*/}
-                                            {/*</div>*/}
+
                                         </div>
                                     </div>
                                     :

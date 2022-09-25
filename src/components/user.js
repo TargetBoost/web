e38d54c4b27e2ba594eb3ee0dca6f9233883f233
Андрей Шмелев-Шампанов <andreyshsh@MacBook-Air-Andrej.local>
2022-09-25 05:53:30 +0300

    add admin

--- src/components/user.js
+++ src/components/user.js
@@ -328,7 +328,7 @@ class User extends Component{
                                                     <div className="block-default-pre">
                                                         <div className="task-wall">
                                                             {
-                                                                this.state.targets.length > 0 ?
+                                                                filterTarget(this.state.targets, "end").length > 0 ?
                                                                     filterTarget(this.state.targets, "end").map(t =>
 
 

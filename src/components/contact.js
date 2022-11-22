95e1042f6d37484983ae5cd585dd5dba5249741c
AndreySHSH <laptev.andrey@icloud.com>
2022-11-22 23:09:27 +0300

    test

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import ReactJWPlayer from "react-jw-player";
 import target from "../icon/target.png"
 
 class Contact extends Component{
@@ -6,8 +7,44 @@ class Contact extends Component{
         super(props);
         this.state = {
             targetAction: "sign-in",
+            advertising: {
+                admessage: "This video will resume in xx seconds",
+                adscheduleid: "your_ad_schedule_id",
+                client: "vast",
+                cuetext: "Advertisement",
+                outstream: false,
+                preloadAds: false,
+                vpaidcontrols: false,
+                rules: {
+                    startOnSeek: "pre",
+                    timeBetweenAds: 0
+                },
+                schedule: [{
+                    tag: [
+                        "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]"
+                    ],
+                    type: "linear",
+                    offset: 25
+                },
+                    {
+                        tag: [
+                            "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]"
+                        ],
+                        type: "linear",
+                        offset: 125
+                    },
+                    {
+                        tag: [
+                            "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]"
+                        ],
+                        type: "linear",
+                        offset: 225
+                    }]
+            }
         }
     }
+
+
 
     render() {
         return (
@@ -28,6 +65,14 @@ class Contact extends Component{
                         {/*    <img className="default-icon" src={target} alt="target"/>*/}
                         {/*</div>*/}
                     </div>
+                </div>
+                <div className="block-default-pre">
+                    <ReactJWPlayer
+                        file="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                        playerId="123"
+                        playerScript="https://cdn.jwplayer.com/libraries/someid.js"
+                        customProps={{ advertising: { ...this.advertising } }}
+                    />
                 </div>
                 <div className="block-default-pre">
                     <div className="end">

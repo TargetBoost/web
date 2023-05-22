1a6abed68a4be81de049e3d160139e176a192d11
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-23 02:50:30 +0300

    add wallet

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -235,6 +235,9 @@ class Tasks extends Component{
                                                                     :
                                                                         null
                                     }
+                                    <div className="block-default-pre" style={{fontSize: "13px", background: "#f2e4a8"}}>
+                                        Напишите нашему боту (<a href="https://t.me/targetBoostBot" target="_blank" className="underline">@targetBoostBot</a>) в телеграм, что бы начать выполнять задания.
+                                    </div>
                                     <div className="block-default-pre" style={{fontSize: "12px", background: "#f2e4a8"}}>
                                         Не обновляйте страницу, задания будут появлятся автоматически.
                                     </div>

b41f07796a18a7e37cef40e751a76e18cd4c2e44
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-22 15:49:14 +0300

    add wallet

--- src/components/admin.js
+++ src/components/admin.js
@@ -249,7 +249,7 @@ class Admin extends Component{
                                                             this.state.targets.map(t =>
                                                                 <div className="task-item">
                                                                     <div className="task-item-value">{t.login}</div>
-                                                                    {/*<div className="task-item-value">{t.title}</div>*/}
+                                                                    <div className="task-item-value">{(parseInt(store.user.balance)).toLocaleString('ru') } ₽</div>
                                                                     {/*<div className="task-item-value">{t.count}/{t.total}</div>*/}
                                                                     {/*<div className="task-item-value">{ (parseInt(t.total_price)).toLocaleString('ru') } ₽</div>*/}
                                                                     <div className="task-item-value">

4fba2e2f052a2c8942332c80ace351d1d294c4cf
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-22 16:38:51 +0300

    add wallet

--- src/components/targets.js
+++ src/components/targets.js
@@ -153,7 +153,7 @@ class Targets extends Component{
                         store.user.auth === false || store.user.execute === true || store.user.block === true ?
                             store.user.block === true ?
                                 <div className="wrapper-error">
-                                    <div className="error">Ваш профиль был заблокирован по решению администрации.</div>
+                                    <div className="error">Ваш профиль был заблокирован по решению Администрации сайта.</div>
                                     <div className="error small-text">Причина: {store.user.cause}</div>
                                     <br/>
                                     <div className="error small-text">

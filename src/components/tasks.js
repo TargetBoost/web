6b805e2f579aa63b14e46a31a18f7a6d209aab82
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-24 00:38:15 +0300

    test

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -376,7 +376,7 @@ class Tasks extends Component{
                                                                         null
                                     }
                                     <div className="block-default-pre" style={{fontSize: "13px", background: "#f2e4a8"}}>
-                                        Напишите нашему боту (<a href="https://t.me/targetBoostBot" target="_blank" className="underline">@targetBoostBot</a>) в телеграм, что бы начать выполнять задания.
+                                        Напишите нашему боту (<a href="https://t.me/targetBoostBot" target="_blank" className="underline">@targetBoostBot</a>) в телеграм, чтобы начать выполнять задания.
                                     </div>
                                     {/*<div className="block-default-pre" style={{fontSize: "12px", background: "#f2e4a8"}}>*/}
                                     {/*    Не обновляйте страницу, задания будут появлятся автоматически.*/}

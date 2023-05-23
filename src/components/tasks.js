6db40894c994e0999742a4937194bc76d775d3d3
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-05-23 23:59:00 +0300

    test

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -105,7 +105,7 @@ class Tasks extends Component{
             .then(res => {
                 if (res.status.message !== null) {
                     this.state.store.dispatch({
-                        type: "set_error", value: "Ошибка проверки",
+                        type: "set_error", value: "Мы не смогли проверить выполнения задания, попробуйте еще раз.",
                     })
 
                     fetch(`/core/v1/service/executor/target`, {

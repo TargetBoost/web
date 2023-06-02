e19cef9923be2297370e55d16213bc88c8607bc6
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-06-02 04:57:36 +0300

    add video

--- src/components/admin.js
+++ src/components/admin.js
@@ -605,8 +605,9 @@ class Admin extends Component{
                                                                     filterUser(this.state.targets, true).map(t =>
                                                                         <div className="task-item">
                                                                             <div className="task-item-value">ID: {t.id}</div>
-                                                                            <div className="task-item-value">login: {t.login}</div>
+                                                                            <div className="task-item-value">tg: {t.tg}</div>
                                                                             <div className="task-item-value">balance: {(parseInt(t.balance)).toLocaleString('ru') } ₽</div>
+                                                                            <div className="task-item-value">execute: { t.execute.toString() }</div>
                                                                             {/*<div className="task-item-value">{t.count}/{t.total}</div>*/}
                                                                             {/*<div className="task-item-value">{ (parseInt(t.total_price)).toLocaleString('ru') } ₽</div>*/}
                                                                             <div className="task-item-value">

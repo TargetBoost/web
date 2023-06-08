fc4135e55d75f7ce7edcf68f2d6b3d10d89e8cd4
Андрей Шмелев-Шампанов <a.shmelev-shampanov@rotec.digital>
2023-06-08 03:46:25 +0300

    fix login and registration

--- src/components/settings.js
+++ src/components/settings.js
@@ -56,26 +56,31 @@ class Settings extends Component{
                                     <div>VK</div>
                                     {
                                         store.user.vkToken === "" ?
-                                            <div style={{
-                                                width: "200px"
-                                            }}>
-                                                <div className="button-default" onClick={()=>{
-                                                    window.location.href = `https://oauth.vk.com/authorize?client_id=51666148&display=page&redirect_uri=https://targetboost.ru/core/v1/callback_vk&scope=groups,offline&response_type=code&v=5.131&state=${window.localStorage.getItem("token")}`
-                                                }}>Авторизоваться VK</div>
+                                            <div className="wrapper-input">
+                                                <div style={{
+                                                    width: "200px"
+                                                }}>
+                                                    <div className="button-default" onClick={()=>{
+                                                        window.location.href = `https://oauth.vk.com/authorize?client_id=51666148&display=page&redirect_uri=https://targetboost.ru/core/v1/callback_vk&scope=groups,offline&response_type=code&v=5.131&state=${window.localStorage.getItem("token")}`
+                                                    }}>Авторизоваться VK</div>
+                                                </div>
                                             </div>
                                         :
                                             <div style={{
                                                 width: "200px"
 
                                             }}>
-                                                {store.user.vkUserFirstName}
-                                                {store.user.vkUserLastName}
-                                                <div style={{
-                                                    width: "200px"
-                                                }}>
-                                                    <div className="button-default" onClick={()=>{
-                                                        window.location.href = `https://oauth.vk.com/authorize?client_id=51666148&display=page&redirect_uri=https://targetboost.ru/core/v1/callback_vk&scope=groups,offline&response_type=code&v=5.131&state=${window.localStorage.getItem("token")}`
-                                                    }}>Авторизоваться заново</div>
+                                                <div className="wrapper-input">
+                                                    {store.user.vkUserFirstName} {store.user.vkUserLastName}
+                                                </div>
+                                                <div className="wrapper-input">
+                                                    <div style={{
+                                                        width: "200px"
+                                                    }}>
+                                                        <div className="button-default" onClick={()=>{
+                                                            window.location.href = `https://oauth.vk.com/authorize?client_id=51666148&display=page&redirect_uri=https://targetboost.ru/core/v1/callback_vk&scope=groups,offline&response_type=code&v=5.131&state=${window.localStorage.getItem("token")}`
+                                                        }}>Авторизоваться заново</div>
+                                                    </div>
                                                 </div>
                                             </div>
 

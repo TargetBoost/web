e09b2c1e7e714d7a07c5405182ea636aee180af6
Андрей Шмелев-Шампанов <andrey_shsh@Air-Andrej.Dlink>
2023-06-09 23:23:38 +0300

    test

--- src/components/contact.js
+++ src/components/contact.js
@@ -64,12 +64,6 @@ class Contact extends Component{
                                 </div>
                             </div>
                             <div className="block-default-pre">
-                                {/*<video onClick={this.handleToggleVideo}  src="//" className="video-thumbnail" controls=""*/}
-                                {/*       disablePictureInPicture="" preload="none"*/}
-                                {/*       poster="https://samplelib.com/lib/preview/mp4/sample-5s.jpg"*/}
-                                {/*       controlsList="nodownload"></video>*/}
-                            </div>
-                            <div className="block-default-pre">
                                 <div className="end">
                                     Основатель<br/>
                                     <a className="button-text" target="_blank" href="https://t.me/andrey_shsh">Андрей</a>

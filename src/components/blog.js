5949d4d52d363e5907a6bc4514e9693fb8fc3d0e
Андрей Шмелев-Шампанов <andrey_shsh@Air-Andrej.Dlink>
2023-06-22 03:04:03 +0300

    test comments

--- src/components/blog.js
+++ src/components/blog.js
@@ -591,7 +591,7 @@ class Blog extends Component{
                                                                 </div>
                                                             </div>
                                                         :
-                                                            <div className="comment-wrapper"  style={{display: "flex", padding: "10px", borderRadius: "20px", width: "100%"}}>
+                                                            <div className="comment-wrapper"  style={{display: "flex", padding: "10px", borderRadius: "20px", width: "100%"}} onClick={()=> this.setState({isParent: true, parent: c})}>
                                                                 <div style={{marginRight: "10px", padding: "8px"}}>
                                                                     {
                                                                         c.main_image !== "" ?

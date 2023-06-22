1a7faca15d595902e04940538068c9d1ab00b043
Андрей Шмелев-Шампанов <andrey_shsh@Air-Andrej.Dlink>
2023-06-22 11:17:07 +0300

    test parent

--- src/components/blog.js
+++ src/components/blog.js
@@ -618,7 +618,7 @@ class Blog extends Component{
                                                 this.state.isParent ?
                                                     <div style={{display: "flex", padding: "10px", background: "#fafafa", width: "100%", margin: "5px 0 5px 0", wordBreak: "break-all"}}>
                                                         <div>
-                                                            <img src={re} style={{marginRight: "10px", background: "#dcdcdc", maxWidth: "40px"}}  alt="re"/>
+                                                            <img src={re} style={{marginRight: "10px", maxWidth: "40px"}}  alt="re"/>
                                                         </div>
                                                         <div style={{marginRight: "10px", background: "#dcdcdc", width: "3px"}}>
 
